406aba321ad662a90f2fdb6bdd8685ce7eb0de72
Chow Minh Khang <minhkhang@lozi.vn>
2024-10-30 20:12:54 +0700

    fix bug show duplicate class

--- src/ClassList.cpp
+++ src/ClassList.cpp
@@ -5,25 +5,32 @@ using namespace std;
 
 ClassList::ClassList() : classCount(0) {}
 
-void ClassList::addClass(const Lop& newClass) {
-    if (classCount < 1000) {
+void ClassList::addClass(const Lop &newClass)
+{
+    if (classCount < 1000)
+    {
         classes[classCount++] = new Lop(newClass); // Giả sử Lop có constructor
-    } else {
+    }
+    else
+    {
         cout << "Danh sách lớp đã đầy!" << endl;
     }
 }
 
-void ClassList::printClasses() {
-    for (int i = 0; i < classCount; i++) {
-        cout << "Mã lớp: " << classes[i]->getClassID() 
-             << ", Tên lớp: " << classes[i]->getClassName() << endl; // Sử dụng getter
+void ClassList::printClasses()
+{
+    for (int i = 0; i < classCount; i++)
+    {
         classes[i]->printClassInfo(); // In thông tin lớp
     }
 }
 
-void ClassList::updateClass(const string& malop, const Lop& updatedClass) {
-    for (int i = 0; i < classCount; i++) {
-        if (classes[i]->getClassID() == malop) { // Sử dụng getter
+void ClassList::updateClass(const string &malop, const Lop &updatedClass)
+{
+    for (int i = 0; i < classCount; i++)
+    {
+        if (classes[i]->getClassID() == malop)
+        {                               // Sử dụng getter
             *classes[i] = updatedClass; // Cập nhật thông tin lớp
             cout << "Cập nhật lớp " << malop << " thành công!" << endl;
             return;
@@ -32,9 +39,12 @@ void ClassList::updateClass(const string& malop, const Lop& updatedClass) {
     cout << "Không tìm thấy lớp " << malop << "." << endl;
 }
 
-void ClassList::addStudentToClass(const string& malop, const SinhVien& newStudent) {
-    for (int i = 0; i < classCount; i++) {
-        if (classes[i]->getClassID() == malop) { // Sử dụng getter
+void ClassList::addStudentToClass(const string &malop, const SinhVien &newStudent)
+{
+    for (int i = 0; i < classCount; i++)
+    {
+        if (classes[i]->getClassID() == malop)
+        {                                       // Sử dụng getter
             classes[i]->addStudent(newStudent); // Thêm sinh viên thông qua phương thức
             cout << "Thêm sinh viên " << newStudent.getMaSV() << " vào lớp " << malop << " thành công!" << endl;
             return;
